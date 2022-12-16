d3dfa71dfe657bb48353a8e445b662b42fc34404
4rievilome <isabella.nunes@dcx.ufpb.br>
2022-12-16 20:32:21 -0300

    header/nav

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/* SHOW MENU */
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/* MENU SHOW */
+/* Valida se a constante existe */
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* MENU HIDDEN */
+/* Valida se a constante existe */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelector('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // Quando clicamos no nav__link, removemos o show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.add.addEventListener('click', linkAction))
