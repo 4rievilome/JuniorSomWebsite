ed71959bc47d1db1e975126f6054dfded83a487d
4rievilome <isabella.nunes@dcx.ufpb.br>
2022-12-17 01:11:32 -0300

    finalização da home mobile com js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
-/* SHOW MENU */
+/*=== SHOW MENU ===*/
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
-/* MENU SHOW */
-/* Valida se a constante existe */
+/*=== MENU SHOW ===*/
+/*=== Valida se a constante existe ===*/
 if(navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-/* MENU HIDDEN */
-/* Valida se a constante existe */
+/*=== MENU HIDDEN ===*/
+/*=== Valida se a constante existe ===*/
 if(navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
-/* REMOVE MENU MOBILE */
+/*=== REMOVE MENU MOBILE ===*/
 const navLink = document.querySelector('.nav__link')
 
 function linkAction() {
@@ -28,3 +28,10 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.add.addEventListener('click', linkAction))
+
+/*=== MUDAR BACKGROUND HEADER ===*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-eader'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
